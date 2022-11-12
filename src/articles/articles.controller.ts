e7973f5c334b29914dea2c6b7570723a8ee77a0b
unknown <halaszbalazs9825@gmail.com>
2022-11-12 14:22:33 +0100

    Implementing Article CRUD

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -7,33 +7,40 @@ import {
   Param,
   Delete,
   NotFoundException,
+  ParseIntPipe,
 } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ArticleEntity } from './entities/article.entity';
 
 @Controller('articles')
+@ApiTags('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
   }
 
   @Get()
+  @ApiCreatedResponse({ type: ArticleEntity, isArray: true })
   findAll() {
     return this.articlesService.findAll();
   }
 
   @Get('drafts')
+  @ApiCreatedResponse({ type: ArticleEntity, isArray: true })
   findAllDrafts() {
     return this.articlesService.findAllDrafts();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const article = await this.articlesService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const article = await this.articlesService.findOne(id);
 
     if (!article) {
       throw new NotFoundException('article not found');
@@ -43,12 +50,17 @@ export class ArticlesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articlesService.update(+id, updateArticleDto);
+  @ApiCreatedResponse({ type: ArticleEntity })
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articlesService.update(id, updateArticleDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.articlesService.remove(+id);
+  @ApiCreatedResponse({ type: ArticleEntity })
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.articlesService.remove(id);
   }
+}
+function ApiCreateResponse() {
+  throw new Error('Function not implemented.');
 }
